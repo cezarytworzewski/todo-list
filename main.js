4ae442c21a831ceba699896d442664a895517f70
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2018-01-25 12:08:44 +0100

    Add to properties delete current or choose task

--- main.js
+++ main.js
@@ -1,61 +1,39 @@
 // - (UI-user interface) input
 // - (UI) lista - dodawanie elementów listy
 // - (Logic) manager - będzie łączył input z listą
+const ENTER_KEY_CODE = 13;
 
 class Manager {
   constructor() {
-    const $list = document.querySelector('.todo-list');
-    // const $input = document.querySelector('.todo-list-input');
-    // poniższa wartość jest również dobrze zdefiniowana
-
-    // 'this.$input' to nowo utworzona zmienna, czyli dodanie przez
-    //referencje klasy '.todo-list-input' z modelu DOM.
+    this.$list = document.querySelector('.todo-list');
     this.$input = document.querySelector('.todo-list-input');
-    this.$list = $list;
+    this.$button = document.querySelector('.add-button-to-list');
   }
 
-  setup() { //funkcja, która wywoła się, że po kliknięciu przycisku Enter
-            //przypisze nam funkcje render(), która dodaje wartość do listy
-    const self = this; //przypisujemy this, jako self
-
-    //odwołuje się przez referencje jako 'this.$input' i nakazuje, aby
-    //przycisk INPUT nasłuchwiał na wciśnięcie przyciska 'keydown'
-    this.$input.addEventListener('keydown', function (event) {
-      //tutaj nasluchujemy, że jeśli naciśnimy Enter
-      if (event.keyCode === 13) {
-
-        //to wywołamy funkcje render,
-        self.render();
+  setup() {
+    this.$input.addEventListener('keydown', (event) => {
+      if (event.keyCode === ENTER_KEY_CODE) {
+        this.render();
       }
     });
+    this.$button.addEventListener('click', () => this.render());
   }
 
   render() {
-
-    //funkcja render tworzy zmienną wartość (value)--> wskazuje, że konkretny
-    // input, czyli 'this.$input' oczekuje wartości 'this.$input.value;' , gdzie
-    //value to utworzenie tak na prawdę  value w modelu DOM.
-    const value = this.$input.value;
-    console.log(value);
-
-    //poniżej tworzę stałą $li, która dodaje/tworzy elementy 'li'
     const $li = document.createElement('li');
-
-    //poniżej pobieramy/ustawiamy wartość tekstową na value
-    $li.textContent = value;
-
-    //poniżej, dzięki metodzie appendChil wstawiamy dynamicznie element 'li'
+    $li.textContent = this.$input.value;
     this.$list.appendChild($li);
-
-    // domyślnie wartość inputa to pusty string
+    this.deleteTask($li);
     this.$input.value = '';
   }
+
+  deleteTask($li) {
+    const $x = document.createElement('button');
+    $x.style.border = 'white solid';
+    $x.textContent = '    x';
+    $li.appendChild($x);
+    $x.addEventListener('click', function () {
+      $li.parentNode.removeChild($li);
+    });
+  }
 }
-
-// nasłuchiwanie przez przeglądarkę na zdarzenie
-window.addEventListener('load', function () {
-  const manager = new Manager(); // podczas tworzenie obiektu Manager wywołuje
-  //konstruktor tej klasy
-  manager.setup();
-
-});
